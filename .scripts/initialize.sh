04d6c494c996ec99b66423dbaaa253a53e40d63a
Adharsh Babu <adharsh.babu@gmail.com>
2023-06-01 13:07:14 -0700

    ripgrep

--- .scripts/initialize.sh
+++ .scripts/initialize.sh
@@ -23,6 +23,6 @@ sudo apt install -y caffeine
 ~/dotfiles/.scripts/caffeine-indicator-fix.sh
 
 # Other installs
-sudo apt install -y i3 silversearcher-ag maim xclip stow udiskie blueman
+sudo apt install -y i3 silversearcher-ag maim xclip stow udiskie blueman ripgrep
 
 echo "Reboot to see changes."
