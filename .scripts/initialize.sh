0bb7790d7ea34bd50bc456940df88ad6bf7705af
Adharsh Babu <adharsh.babu@gmail.com>
2023-06-03 23:16:58 -0700

    Update initialize.sh

--- .scripts/initialize.sh
+++ .scripts/initialize.sh
@@ -9,7 +9,8 @@ set -e
 sudo apt install -y brightnessctl
 sudo usermod -aG video $USER
 
-# https://github.com/alols/xcape
+# Linux: https://github.com/alols/xcape
+# Windows: https://gist.github.com/tanyuan/55bca522bf50363ae4573d4bdcf06e2e
 sudo apt install -y git gcc make pkg-config libx11-dev libxtst-dev libxi-dev
 git clone https://github.com/alols/xcape.git ~/.xcape
 (cd ~/.xcape && make && sudo make install)
