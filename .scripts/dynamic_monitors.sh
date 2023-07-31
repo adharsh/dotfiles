f58c2ebfac72e6ad7d1a7adbea48e77ce8dd2f64
Adharsh Babu <adharsh.babu@gmail.com>
2023-07-30 22:34:12 -0700

    Huawei specific changes

--- .scripts/dynamic_monitors.sh
+++ .scripts/dynamic_monitors.sh
@@ -7,7 +7,7 @@ second_monitor=$(xrandr --query | rg ' connected ' | tail -1 | awk '{print $1}')
 
 if [ "$second_monitor" != "eDP-1" ]; then
   # Change monitor outputs with xrandr
-  xrandr --output "$second_monitor" --auto --above eDP-1
+  xrandr --output eDP-1 --primary --scale 1.28x1.28 --pos 0x2160 --rotate normal --output $second_monitor --scale 2x2
 
   i3-msg "workspace 1, move workspace to output $second_monitor"
   i3-msg "workspace 2, move workspace to output $second_monitor"
