b84ce36f2405f5198059897077d7b6d75a5174d3
Adharsh Babu <adharsh.babu@gmail.com>
2023-08-05 10:19:55 -0700

    Update dynamic_monitors.sh

--- .scripts/dynamic_monitors.sh
+++ .scripts/dynamic_monitors.sh
@@ -12,8 +12,9 @@ if [ "$second_monitor" != "eDP-1" ]; then
   i3-msg "workspace 1, move workspace to output $second_monitor"
   i3-msg "workspace 2, move workspace to output $second_monitor"
   i3-msg "workspace 4, move workspace to output $second_monitor"
+  i3-msg "workspace 6, move workspace to output $second_monitor"
+  i3-msg "workspace 8, move workspace to output $second_monitor"
   i3-msg "workspace 10, move workspace to output $second_monitor"
 else
   xrandr --auto
 fi
-
