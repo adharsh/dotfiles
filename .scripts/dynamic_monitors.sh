d54d4af6ae0cd9cd9bddf9532b052da7e97ea074
Adharsh Babu <adharsh.babu@gmail.com>
2023-09-02 19:29:25 -0700

    update

--- .scripts/dynamic_monitors.sh
+++ .scripts/dynamic_monitors.sh
@@ -9,7 +9,7 @@ if [ "$second_monitor" != "eDP-1" ]; then
   # Change monitor outputs with xrandr
   xrandr --output eDP-1 --primary --scale 1.28x1.28 --pos 0x2160 --rotate normal --output $second_monitor --scale 2x2
 
-  i3-msg "workspace 1, move workspace to output $second_monitor"
+#  i3-msg "workspace 1, move workspace to output $second_monitor"
   i3-msg "workspace 2, move workspace to output $second_monitor"
   i3-msg "workspace 4, move workspace to output $second_monitor"
   i3-msg "workspace 6, move workspace to output $second_monitor"
