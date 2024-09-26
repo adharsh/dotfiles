31e8f34ad28f96576784df3dbc81d008f3483b61
Adharsh Babu <adharsh.babu@gmail.com>
2024-09-26 16:25:53 -0700

    Simplify center_mouse.sh

--- .scripts/center_mouse.sh
+++ .scripts/center_mouse.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-XTE=/usr/bin/xte
+# Get focused window
+WINDOW=$(xdotool getwindowfocus)
 
-WINDOW=`xdotool getwindowfocus`
-
-# Get the window geometry using xdotool
-eval $(xdotool getwindowgeometry --shell $WINDOW)
-
-# Calculate the center of the window
-TX=$((WIDTH / 2))
-TY=$((HEIGHT / 2))
-
-# Calculate the desired mouse coordinates relative to the center of the window
-MOUSE_X=$((X + TX))
-MOUSE_Y=$((Y + TY))
-
-# Move the mouse to the calculated coordinates using xte
-$XTE "mousemove $MOUSE_X $MOUSE_Y"
+# Move mouse to the center of the focused window
+xdotool mousemove --window $WINDOW --polar 0 0
