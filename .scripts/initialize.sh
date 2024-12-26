ec9cfac02fb26a7f78a84b8cc8e5ac51e3086b8a
Adharsh Babu <adharsh.babu@gmail.com>
2024-12-26 14:43:27 -0500

    update

--- .scripts/initialize.sh
+++ .scripts/initialize.sh
@@ -202,4 +202,12 @@ chmod +x ~/bin/bazel
 # HyperSnips Markdown
 ln -s ~/dotfiles/.scripts/markdown.hsnips ~/.config/Code/User/globalStorage/draivin.hsnips/hsnips/markdown.hsnips
 
+# Scrcpy, screen capture for Android
+sudo apt install ffmpeg libsdl2-2.0-0 adb wget \
+                 gcc git pkg-config meson ninja-build libsdl2-dev \
+                 libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev \
+                 libswresample-dev libusb-1.0-0 libusb-1.0-0-dev
+git clone https://github.com/Genymobile/scrcpy ~/.scrcpy
+(cd ~/.scrcpy && ./install_release.sh)
+
 read -p "Reboot to see changes." -r
