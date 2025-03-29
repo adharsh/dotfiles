519ea906a500f4a83080f286a889ab85bff54592
Adharsh Babu <adharsh.babu@gmail.com>
2025-03-29 10:48:38 -0700

    update

--- .scripts/restart_chrome_password_sync.sh
+++ .scripts/restart_chrome_password_sync.sh
@@ -1,6 +1,6 @@
-echo "Contents of $0"
-cat $0
-read -p "Verify if profile is correct. Check if sync is wrong from: chrome://sync-internals/. Find directory from: chrome://version."
+#!/bin/bash
+
+read -rp "Verify if profile is correct. Check if sync is wrong from: chrome://sync-internals/. Find \"Profile Path\" directory from: chrome://version."
 
 # Check if this is the right profile. Currently set to default chrome profile.
 cd /home/adharsh/.config/google-chrome/Default
